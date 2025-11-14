dcae2b133fb7076570728970e6d5af16024257cc
Mohamed Aymn <mohamedaymn218@gmail.com>
2025-11-14 18:01:25 +0200

    chore: contact form dropdown elements

--- frontend/src/ts/modals/contact.ts
+++ frontend/src/ts/modals/contact.ts
@@ -1,9 +1,34 @@
+import SlimSelect from "slim-select";
 import AnimatedModal from "../utils/animated-modal";
 
+let select: SlimSelect | undefined = undefined;
+
 export function show(): void {
-  void modal.show();
+  void modal.show({
+    beforeAnimation: async (modalEl) => {
+      select = new SlimSelect({
+        select: modalEl.querySelector(
+          "select[name='contactType']"
+        ) as HTMLElement,
+        data: [
+          { text: "Question", value: "Question" },
+          { text: "Feedback", value: "Feedback" },
+          { text: "Bug Report", value: "Bug Report" },
+          { text: "Suggestion", value: "Suggestion" },
+          { text: "Other", value: "Other" },
+        ],
+        settings: {
+          contentLocation: modalEl,
+        },
+      });
+    },
+  });
 }
 
 const modal = new AnimatedModal({
   dialogId: "contactModal",
+  cleanup: async (): Promise<void> => {
+    select?.destroy();
+    select = undefined;
+  },
 });
